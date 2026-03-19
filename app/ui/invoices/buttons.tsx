6286217d2f54a5bfbe907a7b3be42164c386daf2
Med Elmostapha <dedijekani@gmail.com>
2026-03-19 01:58:11 +0000

    fix(ui): use server action directly in DeleteInvoice form

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -29,11 +29,7 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
   return (
-    <form
-      action={async () => {
-        await deleteInvoiceWithId();
-      }}
-    >
+    <form action={deleteInvoiceWithId}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
