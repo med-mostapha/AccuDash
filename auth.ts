17c66f03cea32b5d8a26d77478ae24b5a33302cf
Med Elmostapha <dedijekani@gmail.com>
2026-03-19 01:57:46 +0000

    refactor(auth): migrate getUser to use @vercel/postgres

--- auth.ts
+++ auth.ts
@@ -2,17 +2,16 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
-import postgres from "postgres";
+import { sql } from "@vercel/postgres";
 import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
-
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return user[0];
+    const result = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return result.rows[0];
   } catch (error) {
+    console.error("Failed to fetch user:", error);
     throw new Error("Failed to fetch user.");
   }
 }
